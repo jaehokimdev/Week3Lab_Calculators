c8e5320d6727902a26b8042d28b62c1211168357
jaehokimdev <101899896+jaehokimdev@users.noreply.github.com>
2022-09-20 20:46:57 -0600

    Create agecalculator.jsp and servlets files

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,35 @@
+
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Joe Kim
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculators.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String age = request.getParameter("age");
+        
+        request.setAttribute("age", age);
+        
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculators.jsp").forward(request, response);
+
+    }
+
+}
---
+++ web/WEB-INF/agecalculators.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : agecalculators
+    Created on : Sep 20, 2022, 8:22:58 PM
+    Author     : Joe Kim
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        <form method="post" action="age">
+        Enter your age: <input type="text" name="age"><br>
+        <input type="submit" value="Age next birthday"><br>
+        Arithmetic Calculator
+        </form>
+    </body>
+</html>
